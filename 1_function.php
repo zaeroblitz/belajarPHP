70717097e97204baecefc211614b31f0cada18cc
zaeroblitz <zaeroblitz@gmail.com>
2021-03-23 07:44:58 +0700

    Test function location

--- 1_function.php
+++ 1_function.php
@@ -1,14 +1,28 @@
 <?php 
 
 function sayHello(){
-    echo "This is how to make function in PHP";
+    echo "This is how to make function in PHP\n";
 }
 
 sayHello();
 
 function testName(String $name) {
-    echo "Hello $name welcome to PHP";
+    echo "Hello $name welcome to PHP\n";
 }
 testName('Zaero Blitz');
+
+$test = false;
+
+if ($test) {
+    function hi() {
+        echo "Hi\n";
+    }
+} else {
+    function hi() {
+        echo "False\n";
+    }
+}
+
+hi();
 
 ?>
