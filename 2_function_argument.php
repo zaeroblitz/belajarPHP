cd6858c3e78413bd1224882eb79bba6f93959ba5
zaeroblitz <zaeroblitz@gmail.com>
2021-03-23 07:55:26 +0700

    Add Function Argument

---
+++ 2_function_argument.php
@@ -0,0 +1,16 @@
+<?php 
+
+function sayHello($name) {
+    echo "Hello $name";
+}
+
+sayHello("Ucok CJ \n");
+sayHello("Tole \n");
+
+function sayHi($name = "Zero Blitz") {
+    echo "Hi $name \n";
+}
+
+sayHi();
+sayHi('Sento Isuzu');
+?>
