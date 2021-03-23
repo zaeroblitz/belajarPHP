aaff5ba78ba2cec4ab62e4139da7dc56ba98c266
zaeroblitz <zaeroblitz@gmail.com>
2021-03-23 08:07:29 +0700

    Variable-length Argument List

--- 2_function_argument.php
+++ 2_function_argument.php
@@ -8,9 +8,29 @@ sayHello("Ucok CJ \n");
 sayHello("Tole \n");
 
 function sayHi($name = "Zero Blitz") {
-    echo "Hi $name \n";
+    echo <<<NAME
+    Hello $name \n
+    NAME;
 }
 
 sayHi();
 sayHi('Sento Isuzu');
+
+function sum(int $a, int $b) {
+    $total = $a + $b;   
+    echo "Total = $total\n";
+}
+sum(10, 20);
+sum(1.2, 2.1);
+
+function sumAll(...$values) {
+    $total = 0;
+    foreach($values as $value) {
+        $total += $value;
+    }
+    echo "Total " . implode(" + ", $values) . " = $total \n";
+}
+
+sumAll(1, 2, 40, 50, 90);
+sumAll(...[1, 40, 23, 45, 34, 89]);
 ?>
