11c9d6d66fa945bc9d0f923a4d4025ce58fa6222
zaeroblitz <zaeroblitz@gmail.com>
2021-03-24 20:14:20 +0700

    Add Inheritence

---
+++ php_oop/2_inheritence.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+    class Monster {
+        var string $monsterName = "No Name";
+        var string $attack = "Normal Attack";
+        var int $damage = 30;
+
+        function attack() {
+            echo "{$this->monsterName} is attacking you with {$this->attack} taken {$this->damage} heatlh damage";
+        }
+
+        public function __construct(string $monsterName, string $attack) {
+            $this->monsterName = $monsterName;
+            $this->attack = $attack;
+        }
+    }
+
+    class StoneMonster extends Monster {}
+
+    $zubat = new Monster("Zubat", "Wing Attack");
+    $golem = new StoneMonster("Golem", "Earthquake");
+?>
+<body>
+    
+    <p>
+        <?php 
+            echo "{$zubat->attack()}" ?>
+    </p>
+
+    <p>
+        <?php 
+            echo "{$golem->attack()}" ?>
+    </p>
+
+</body>
+</html>
