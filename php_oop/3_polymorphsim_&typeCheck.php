f981ac4bb1a264dbe8b9f154afbe1787bbb1f9df
zaeroblitz <zaeroblitz@gmail.com>
2021-03-25 08:24:57 +0700

    Updated Polymorphsim & Type Check 2

--- php_oop/3_polymorphsim_&typeCheck.php
+++ php_oop/3_polymorphsim_&typeCheck.php
@@ -26,7 +26,21 @@
         echo "{$monster->monsterName} is walking and looking around";
     }
 
-    
+    function monsterRun(Monster $monster) {
+        if ($monster instanceof Goblin) {
+            echo "Goblin's {$monster->monsterName} is running to you and calling reinforcement";
+        } else if ($monster instanceof Ogre) {
+            echo "Ogre's {$monster->monsterName} is running to you and preparing spell attack";
+        }
+    }
+
+    function monsterAttack(Monster $monster) {
+        if ($monster instanceof Goblin) {
+            echo "Goblin's {$monster->monsterName} is attacking to you and give 20 health damage";
+        } else if ($monster instanceof Ogre) {
+            echo "Ogre's {$monster->monsterName} is using black magic to you and give 50 healt damage";
+        }
+    }
 ?>
 <body>
     
@@ -34,16 +48,38 @@
         <?php 
             $dungeon = new Dungeon();
 
-            $dungeon->monster = new Goblin("Goblin");
+            $dungeon->monster = new Goblin("Midget");
             monsterWalk($dungeon->monster);
             echo "<br>";
-            monsterWalk(new Goblin("Hob Goblin"));
+            monsterRun($dungeon->monster);
+            echo "<br>";
+            monsterAttack($dungeon->monster);
+            echo "<br>";
             echo "<br>";
             
-            $dungeon->monster = new Ogre("Ogre");
+            monsterWalk(new Goblin("Hob Goblin"));
+            echo "<br>";
+            monsterRun(new Goblin("Hob Goblin"));
+            echo "<br>";
+            monsterAttack(new Goblin("Hob Goblin"));
+            echo "<br>";
+            echo "<br>";
+            
+            $dungeon->monster = new Ogre("Ogre Magi");
             monsterWalk($dungeon->monster);
             echo "<br>";
+            monsterRun($dungeon->monster);
+            echo "<br>";
+            monsterAttack($dungeon->monster);
+            echo "<br>";
+            echo "<br>";
+
             monsterWalk(new Ogre("King Ogre"));
+            echo "<br>";
+            monsterRun(new Ogre("King Ogre"));
+            echo "<br>";
+            monsterAttack(new Ogre("King Ogre"));
+            echo "<br>";
         ?>
     </p>
     
