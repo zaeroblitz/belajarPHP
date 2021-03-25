55e9880fda4c15ecbbe142b7c6a62d1df2d472e2
zaeroblitz <zaeroblitz@gmail.com>
2021-03-25 08:06:29 +0700

    Updated Polymorphsim & Type Check

---
+++ php_oop/3_polymorphsim_&typeCheck.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+    class Dungeon {
+        var Monster $monster;
+    }
+
+    class Monster {
+        public string $monsterName;
+
+        public function __construct(string $monsterName) {
+            $this->monsterName = $monsterName;
+        }
+    }
+
+    class Goblin extends Monster {}
+    class Ogre extends Monster {}
+
+    function monsterWalk(Monster $monster) {
+        echo "{$monster->monsterName} is walking and looking around";
+    }
+
+    
+?>
+<body>
+    
+    <p>
+        <?php 
+            $dungeon = new Dungeon();
+
+            $dungeon->monster = new Goblin("Goblin");
+            monsterWalk($dungeon->monster);
+            echo "<br>";
+            monsterWalk(new Goblin("Hob Goblin"));
+            echo "<br>";
+            
+            $dungeon->monster = new Ogre("Ogre");
+            monsterWalk($dungeon->monster);
+            echo "<br>";
+            monsterWalk(new Ogre("King Ogre"));
+        ?>
+    </p>
+    
+
+</body>
+</html>
